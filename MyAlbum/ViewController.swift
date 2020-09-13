d92b7c79898c54267d788c09f4d27c4a5966a54d
KeunHyeong <rmsgudxx@gmail.com>
2020-09-13 21:38:10 +0900

    Swift Study_1

--- MyAlbum/ViewController.swift
+++ MyAlbum/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentValue = 0
+    
+    @IBOutlet weak var pricaLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        refresh()
     }
-
-
+    
+    @IBAction func btnAction(_ sender: Any) {
+        let message = "가격은 \(currentValue)원 입니다"
+        let alert = UIAlertController(title: "Hello", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler:{ action in
+            self.refresh()
+        })
+        alert.addAction(action)
+        present(alert ,animated: true, completion: nil)
+    }
+    
+    func refresh() {
+        let randomPrice = arc4random_uniform(10000) + 1
+        currentValue = Int(randomPrice)
+        pricaLabel.text = "\(currentValue)"
+    }
 }
 
