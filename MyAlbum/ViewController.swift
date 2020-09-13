8d6ade2f4c532332b4ed0502dcf19d486829be1b
KeunHyeong <rmsgudxx@gmail.com>
2020-09-13 19:26:11 +0900

    Initial Commit

---
+++ MyAlbum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by KeunHyeong on 2020/09/13.
+//  Copyright © 2020 KeunHyeong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
